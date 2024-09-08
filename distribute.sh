7ae69b756fa7c74d17653a81006fd41a5d45171b
lucaayscough <lucaayscough@gmail.com>
2024-09-08 19:53:15 +0100

    Add build to distribution script

--- distribute.sh
+++ distribute.sh
@@ -4,9 +4,14 @@ name="Automate"
 dmg_path="dist/Automate.dmg"
 
 mkdir dist
-codesign -f --deep -o runtime --timestamp -s $codesign_id $vst3_path
-hdiutil create -volname $name -srcfolder $vst3_path -ov -format UDBZ $dmg_path
-codesign -s $codesign_id --timestamp $dmg_path
-xcrun notarytool submit $dmg_path --keychain-profile "notarytool-password" --wait
-xcrun stapler staple $dmg_path 
+mkdir build
+mkdir build/rel
+
+cmake -B build/rel -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" &&
+cmake --build build/rel &&
+codesign -f --deep -o runtime --timestamp -s $codesign_id $vst3_path &&
+hdiutil create -volname $name -srcfolder $vst3_path -ov -format UDBZ $dmg_path &&
+codesign -s $codesign_id --timestamp $dmg_path &&
+xcrun notarytool submit $dmg_path --keychain-profile "notarytool-password" --wait &&
+xcrun stapler staple $dmg_path &&
 xcrun stapler validate $dmg_path
