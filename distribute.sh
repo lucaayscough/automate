bb6b1555f1d9a6cb350bd480df909804fc71f943
lucaayscough <lucaayscough@gmail.com>
2024-09-28 12:07:59 +0100

    Fix track bounds

--- distribute.sh
+++ distribute.sh
@@ -1,4 +1,5 @@
 vst3_path="build/rel/Automate_artefacts/Release/VST3/Automate.vst3"
+vst3_bin_path="build/rel/Automate_artefacts/Release/VST3/Automate.vst3/Contents/MacOS/Automate"
 codesign_id="DJ662GG36K"
 name="Automate"
 dmg_path="dist/Automate.dmg"
@@ -9,6 +10,7 @@ mkdir build/rel
 
 cmake -B build/rel -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" &&
 cmake --build build/rel &&
+llvm-strip $vst3_bin_path &&
 codesign -f --deep -o runtime --timestamp -s $codesign_id $vst3_path &&
 hdiutil create -volname $name -srcfolder $vst3_path -ov -format UDBZ $dmg_path &&
 codesign -s $codesign_id --timestamp $dmg_path &&
