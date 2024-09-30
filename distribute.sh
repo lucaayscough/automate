0e57955f996eaff99e999d6ab8feeb07dcf7d781
lucaayscough <lucaayscough@gmail.com>
2024-09-30 18:24:20 +0100

    add entitlements to hopefully fix plugin loading issues

--- distribute.sh
+++ distribute.sh
@@ -23,8 +23,8 @@ mkdir build/rel
 cmake -B build/rel -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" && cmake --build build/rel &&
 cp -r $vst3_path $vst3_dist_path && cp -r $au_path $au_dist_path &&
 llvm-strip $vst3_bin_path && llvm-strip $au_bin_path &&
-codesign -f --deep -o runtime --timestamp -s $codesign_id $vst3_dist_path &&
-codesign -f --deep -o runtime --timestamp -s $codesign_id $au_dist_path &&
+codesign -f --deep -o runtime --timestamp --entitlements "entitlements.plist" -s $codesign_id $vst3_dist_path &&
+codesign -f --deep -o runtime --timestamp --entitlements "entitlements.plist" -s $codesign_id $au_dist_path &&
 hdiutil create -volname $name -srcfolder "dist/Automate" -ov -format UDBZ $dmg_path &&
 codesign -s $codesign_id --timestamp $dmg_path &&
 xcrun notarytool submit $dmg_path --keychain-profile "notarytool-password" --wait &&
